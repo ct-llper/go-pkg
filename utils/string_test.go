7307863fbabd93b061702a35bc39683c2267f1da
linglu <linglu0212@163.com>
2023-02-28 19:28:51 +0800

    x

---
+++ utils/string_test.go
@@ -0,0 +1,13 @@
+package utils
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test_StringToInt(t *testing.T) {
+	str := "213"
+	err, num := StringToInt(str)
+
+	fmt.Println("=Utils=Test_StringToInt=1=结果=", str, num, err)
+}
